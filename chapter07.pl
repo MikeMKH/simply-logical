e5c1c208b91c3f13297f5cb22e58028be4434f46
Mike Harris <MikeMKH@users.noreply.github.com>
2023-02-22 06:09:23 -0600

    add 7.2

--- chapter07.pl
+++ chapter07.pl
@@ -35,3 +35,39 @@ transitive_verb      --> [beats].
 % S = [the, turtle, beats, the, lazy, turtle] ;
 % S = [the, turtle, beats, the, rapid, turtle] ;
 % S = [the, turtle, beats, the, turtle].
+
+% https://book.simply-logical.space/src/text/3_part_iii/7.2.html
+
+roman(N) -->
+  d(N4,4),
+  d(N3,3),
+  d(N2,2),
+  d(N1,1),
+  {N is N4*1000+N3*100+N2*10+N1}.
+
+i(1) --> [i].
+i(2) --> [x].
+i(3) --> [c].
+i(4) --> [m].
+
+v(1) --> [v].
+v(2) --> [l].
+v(3) --> [d].
+
+d(0,_) --> [].
+d(1,N) --> i(N).
+d(2,N) --> i(N),i(N).
+d(3,N) --> i(N),i(N),i(N).
+d(4,N) --> i(N),v(N).
+%d(4,N) --> i(N),i(N),i(N),i(N).
+d(5,N) --> v(N).
+d(6,N) --> v(N),i(N).
+d(7,N) --> v(N),i(N),i(N).
+d(8,N) --> v(N),i(N),i(N),i(N).
+d(9,N) --> i(N),{N1 is N+1},i(N1).
+
+% ?- phrase(roman(2023), N).
+% N = [m, m, x, x, i, i, i] .
+
+% ?- phrase(roman(1999), N).
+% N = [m, c, m, x, c, i, x] .
